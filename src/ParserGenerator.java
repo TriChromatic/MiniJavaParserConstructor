e436582a6e0f054eea3e65e25e73038861dd53f1
TriChromatic <disulfideprotein@gmail.com>
2014-11-18 11:48:52 -0700

    Small changes to code. Adding better malleability to the code.

--- src/ParserGenerator.java
+++ src/ParserGenerator.java
@@ -4,13 +4,20 @@ import java.util.List;
  * Created by TriChromatic aka Dylan Eicher on 11/17/14.
  */
 public class ParserGenerator {
+    /**
+     * Syntax for parsing the token
+     */
+    final char[] SYNTAX = { '+', '-', '.', '^', '&' };
     private final String GEN_CODE;
 
     /**
      * Generates code from tokens created by delimiter parser
+     * Special syntax includes: +, -, ., ^, &
+     * This code gets messy but hopefully the comments should help anyone who wants to understand it.
+     * I am trying to document my code more and better.
      *
-     * @param tokens
-     * @param removeNull
+     * @param tokens tokens to build parser from
+     * @param removeNull true if adding code to remove null tokens
      */
     ParserGenerator( List<String> tokens, boolean removeNull ) {
         /*Create stringbuilder with proper starting things*/
@@ -21,24 +28,30 @@ public class ParserGenerator {
                 "\tint x = i;\n" +
                 "\tchar c = text.charAt(x);\n" );
 
-        String eq = "=="; //Switches between != and ==
+        /*Helper var declaration*/
+        String eq = "=="; //Switches between != and ==, Defaults to ==
+        String ao = "||"; //Switches between && and ||, Defaults to ||
         int beginIndex = code.length(); //Beginning index where token stuff should start
         int beginIf = code.length(); //Because index where the last if run was conducted
 
-        /*Code generation*/
+        /*Code generation starts here. Loops through tokens*/
         for ( int i = 0; i < tokens.size(); i++ ) {
-            /*Safe token finding*/
-            String token = tokens.get( i );
+            /*Safe token finding. Token is declared final for safety*/
+            final String token = tokens.get( i );
+
+            /*Forward token must only be non syntax related*/
             String forwardToken = "";
             if ( i < tokens.size() - 1 ) {
-                forwardToken = tokens.get( i + 1 );
+                int x = 1;
+                while ( findSyntax( tokens.get( i + x ) ) ) {
+                    forwardToken = tokens.get( i + x );
+                    x++;
+                }
             }
 
-            /*Generate ifs*/
-            if ( !token.startsWith( "+" ) && !token.startsWith( "-" )
-                    && !token.startsWith( "." ) && !token.startsWith( "^" ) ) {
-
-                beginIf = code.length();
+            /*Generate ifs, later statements add modifiers*/
+            if ( !findSyntax( token ) ) {
+                beginIf = code.length(); //Stores where the last if statement began
 
                 /*Construct if char at statements*/
                 if ( eq.equals( "==" ) ) {
@@ -60,12 +73,12 @@ public class ParserGenerator {
                         }
                     }
                 } else {
-                    /*If we use a ^ this is what we do*/
+                    /*If we use a ^ this is what we do use the not eq and and*/
                     code.append( String.format( "\tif(c!='%s'", token.charAt( 0 ) ) );
 
                     if ( token.length() > 1 ) {
                         for ( int j = 1; j < token.length(); j++ ) {
-                            code.append( String.format( "&&c!='%s'", token.charAt( j ) ) );
+                            code.append( String.format( "%sc!='%s'", ao, token.charAt( j ) ) );
                         }
                     }
                     code.append( "){\n" );
@@ -75,16 +88,17 @@ public class ParserGenerator {
                 }
 
                 eq = "=="; //Reset equals
-            } else if ( token.startsWith( "+" ) ) {
+            } else if ( token.charAt( 0 ) == SYNTAX[0] ) {
                 /*Account for ending notation*/
-                code.insert( beginIndex, "\tx = text.length() - 1;\nc = text.charAt(x);\n" );
+                code.insert( beginIndex, "\tx = text.length() - 1;\n" +
+                        "\tc = text.charAt(x);\n" );
                 beginIndex = code.length();
-            } else if ( token.startsWith( "-" ) ) {
+            } else if ( token.charAt( 0 ) == SYNTAX[1] ) {
                 /*Account for starting notation*/
-                code.insert( beginIndex, "\tx = 0;\nc = text.charAt(x);\n" +
-                        "\"" );
+                code.insert( beginIndex, "\tx = 0;\n" +
+                        "\tc = text.charAt(x);\n" );
                 beginIndex = code.length();
-            } else if ( token.startsWith( "." ) ) {
+            } else if ( token.charAt( 0 ) == SYNTAX[2] ) {
                 /*Insert initial thingymabober*/
                 int tempLength = code.length();
                 code.insert( beginIf, String.format( "\tif(c%s'%s'", eq, forwardToken.charAt( 0 ) ) );
@@ -93,16 +107,18 @@ public class ParserGenerator {
                 /*Use forward token and insert the statement into the correct position*/
                 for ( int j = 1; j < forwardToken.length(); j++ ) {
                     tempLength = code.length();
-                    code.insert( tempDex, String.format( " || c%s'%s'", eq, forwardToken.charAt( j ) ) );
+                    code.insert( tempDex, String.format( " %s c%s'%s'", ao, eq, forwardToken.charAt( j ) ) );
                     tempDex = tempDex + ( code.length() - tempLength );
                 }
 
-                /*Insert new x specification*/
-                code.insert( tempDex, "){\n\tx = x - 1;\nc = text.charAt(x);\n" );
+                /*Insert new x specification to look behind*/
+                code.insert( tempDex, "){\n\tx = x - 1;\n\tc = text.charAt(x);\n" );
                 code.append( "}\n" );
                 tokens.remove( forwardToken );
-            } else if ( token.startsWith( "^" ) ) {
+            } else if ( token.charAt( 0 ) == SYNTAX[3] ) {
                 eq = "!=";
+            } else if ( token.charAt( 0 ) == SYNTAX[4] ) {
+                ao = "&&";
             }
         }
 
@@ -110,10 +126,10 @@ public class ParserGenerator {
         if ( removeNull ) {
             code.append( "}\n" );
             code.append( "List cleanTokens = new ArrayList<String>();\n" );
-            code.append( "for ( String s : tokens ) {\n" );
-            code.append( "if ( s != null && s.length() > 0 ) {\n" );
-            code.append( "cleanTokens.add( s );\n" );
-            code.append( "}\n" );
+            code.append( "\tfor ( String s : tokens ) {\n" );
+            code.append( "\t\tif ( s != null && s.length() > 0 ) {\n" );
+            code.append( "\t\tcleanTokens.add( s );\n" );
+            code.append( "\t}\n" );
             code.append( "}\n" );
             code.append( "return cleanTokens;\n" );
             code.append( "}" );
@@ -128,5 +144,14 @@ public class ParserGenerator {
 
     public String getGeneratedCode() {
         return GEN_CODE;
+    }
+
+    private boolean findSyntax( String token ) {
+        for ( char test : SYNTAX ) {
+            if ( token.charAt( 0 ) == test ) {
+                return true;
+            }
+        }
+        return false;
     }
 }
